32cb497c03b5545299025b4e17bc176670505f91
Mandie Quartly <mandie_quartly@uk.ibm.com>
2018-09-20 11:35:20 +0100

    Adding cf files needed to run in IBM Cloud

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn selector_heatmap:server
---
+++ setup.py
@@ -0,0 +1,20 @@
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='eco-frame-public',
+    version='1.0.0',
+    description='Ecosystem Framework',
+    long_description=long_description,
+    url='https://github.com/mandieq/eco-frame-public',
+)
